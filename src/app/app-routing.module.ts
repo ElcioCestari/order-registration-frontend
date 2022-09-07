1f735c546b4a4f14e6dfe3656a60078b2f90e955
ElcioCestari <elciotaira@gmail.com>
2022-09-07 18:30:36 -0300

    feat(main): improve layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductCreateComponent} from "./components/product-create/product-create.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "products",
+    component: ProductCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
