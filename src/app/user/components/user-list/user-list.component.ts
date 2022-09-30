f529fe5f263a283323b3aa07e1592208456f810a
ElcioCestari <elciotaira@gmail.com>
2022-09-29 22:46:18 -0300

    feat(main): init user system list

--- src/app/user/components/user-list/user-list.component.ts
+++ src/app/user/components/user-list/user-list.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { UserSystem } from '../../../core/model/user-system';
+import {MatPaginator, PageEvent} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['username', 'authorities', 'ações'];
+  list = new MatTableDataSource<UserSystem>([]);
+  isMobile: boolean = false;
+  @ViewChild(MatPaginator)private paginator!: MatPaginator;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.list.paginator = this.paginator;
+  }
+
+  delete(element: UserSystem) {}
+
+  edit(element: UserSystem) {}
+
+  newProduct() {}
+
+  pageNavigations($event: PageEvent) {}
 }
