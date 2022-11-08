049d2c39da60e6404c357885978313011145cde0
ElcioCestari <elciotaira@gmail.com>
2022-11-07 22:38:20 -0300

    feat(main): change env from heroku to aws, add port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ec2-52-27-234-52.us-west-2.compute.amazonaws.com'
+  apiUrl: 'https://ec2-52-27-234-52.us-west-2.compute.amazonaws.com:8080'
 };
