115367a2dbba5d2d8944d04d097a52600860118b
ElcioCestari <elciotaira@gmail.com>
2023-03-26 19:54:52 -0300

    feat(main): improve header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../user/service/user.service';
+import {Component, HostListener} from '@angular/core';
+import {UserService} from '../../user/service/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  isLogged = false;
-  constructor(private readonly userService: UserService) {}
+export class HeaderComponent {
+  isLogged: boolean;
 
-  ngOnInit(): void {}
+  constructor(private readonly userService: UserService) {
+    this.isLogged = !!sessionStorage.getItem('authorities');
+  }
 
   logout() {
     this.userService.logout();
+    this.isLogged = false;
+  }
+
+  @HostListener('window:storage', ['$event'])
+  onStorageChange(event: StorageEvent) {
+    if (event.storageArea === sessionStorage && event.key === 'authorities') {
+      this.isLogged = event.newValue === 'true';
+    }
   }
 }
