91c1d78bc786566e1dcc5bff13b0ba490c0d478a
ElcioCestari <elciotaira@gmail.com>
2023-04-06 23:07:22 -0300

    feat(main): add product.module.ts

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,9 +3,6 @@ import {ProductListComponent} from "./components/product-list/product-list.compo
 import {ProductUpdateComponent} from "./components/product-update/product-update.component";
 import {QuantityPatchComponent} from "./components/product-patch/quantity-patch/quantity-patch.component";
 import {CommonModule} from "@angular/common";
-import {HttpClientModule} from "@angular/common/http";
-import {AppRoutingModule} from "../app-routing.module";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MaterialModule} from "../material/material.module";
 import {FormsModule} from "@angular/forms";
 import {ProductCreateComponent} from "./components/product-create/product-create.component";
@@ -20,9 +17,6 @@ import {ProductCreateComponent} from "./components/product-create/product-create
   ],
   imports: [
     CommonModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
     MaterialModule,
     FormsModule
   ]
