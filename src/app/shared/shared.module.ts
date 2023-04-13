3804abb9e7d9398d319796c6d51db28702c9b21f
ElcioCestari <elciotaira@gmail.com>
2023-04-12 22:07:31 -0300

    refactor(feat/form-builder): add shared services to respective module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import {ConfirmDialogComponent} from "./components/confirm-dialog/confirm-dialog.component";
 import {SnackbarService} from "./services/snackbar.service";
 import ErrorMsgService from "./services/error-msg-service";
+import {CategoryService} from "./services/category.service";
+import {AuthInterceptorService} from "./services/auth-interceptor.service";
 
 
 
@@ -15,7 +17,9 @@ import ErrorMsgService from "./services/error-msg-service";
   ],
   providers: [
     SnackbarService,
-    ErrorMsgService
+    ErrorMsgService,
+    CategoryService,
+    AuthInterceptorService
   ]
 })
 export class SharedModule { }
